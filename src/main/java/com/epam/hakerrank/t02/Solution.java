4043f307fc84ddb7ee49ee5d8e67dcaa75ed147f
Ilia Gusevskii <il.gusevskiye@gmail.com>
2017-07-25 16:55:35 +0300

    Test: released tests for numberNeeded method

--- src/main/java/com/epam/hakerrank/t02/Solution.java
+++ src/main/java/com/epam/hakerrank/t02/Solution.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 
 public class Solution {
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String a = in.next();
+        String b = in.next();
+
+        System.out.println(numberNeeded(a, b));
+    }
+
     public static int numberNeeded(String first, String second) {
         // TODO: 25/07/17
         throw new UnsupportedOperationException();
-    }
-
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String a = in.next();
-        String b = in.next();
-        System.out.println(numberNeeded(a, b));
     }
 }
