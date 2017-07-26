25b99b850611a272c324411a374cc8eff4c5ac58
Ilia Gusevskii <il.gusevskiye@gmail.com>
2017-07-26 13:06:59 +0300

    Test: t05 isBalanced() tests added

---
+++ src/test/java/com/epam/hakerrank/t05/SolutionTest.java
@@ -0,0 +1,36 @@
+package com.epam.hakerrank.t05;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class SolutionTest {
+
+    @Test
+    public void testIsBalancedReturnsTrueOnABalancedString1(){
+        String input = "{[()]}";
+
+        boolean result = Solution.isBalanced(input);
+
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void testIsBalancedReturnsTrueOnABalancedString2(){
+        String input = "{{[[(())]]}}";
+
+        boolean result = Solution.isBalanced(input);
+
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void testIsBalancedReturnsFalseOnAnUnbalancedString(){
+        String input = "{[(])}";
+
+        boolean result = Solution.isBalanced(input);
+
+        assertThat(result, is(false));
+    }
+}
