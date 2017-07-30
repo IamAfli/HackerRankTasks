98d9298ad3f52ba27b2d66f23e415a97d24626bf
Ilia Gusevskii <il.gusevskiye@gmail.com>
2017-07-30 19:45:25 +0300

    t09 initial commit

---
+++ src/main/java/com/epam/hakerrank/t09/README.md
@@ -0,0 +1,3 @@
+##Tries: Contacts
+
+[task description](https://www.hackerrank.com/challenges/ctci-contacts)
---
+++ src/main/java/com/epam/hakerrank/t09/Solution.java
@@ -0,0 +1,15 @@
+package com.epam.hakerrank.t09;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int a0 = 0; a0 < n; a0++){
+            String op = in.next();
+            String contact = in.next();
+        }
+    }
+}
