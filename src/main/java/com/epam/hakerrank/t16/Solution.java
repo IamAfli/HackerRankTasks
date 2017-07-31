90070a2f50b9630766da6d0ceb54f7d62b415fb2
Ilia Gusevskii <il.gusevskiye@gmail.com>
2017-07-31 09:13:31 +0300

    t16 solved

--- src/main/java/com/epam/hakerrank/t16/Solution.java
+++ src/main/java/com/epam/hakerrank/t16/Solution.java
@@ -6,7 +6,7 @@ public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int p = in.nextInt();
-        for(int i = 0; i < p; i++){
+        for (int i = 0; i < p; i++) {
             int n = in.nextInt();
 
             boolean result = isPrime(n);
@@ -16,6 +16,18 @@ public class Solution {
     }
 
     static boolean isPrime(int n) {
-        throw new UnsupportedOperationException();
+        if (n == 1){
+            return false;
+        } else if (n == 2){
+            return true;
+        }
+
+        if (n % 2 == 0) return false;
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
     }
 }
