34eb16ea91f9eb054b08054f1ef76c02d7a37a51
Ilia Gusevskii <il.gusevskiye@gmail.com>
2017-07-31 08:10:18 +0300

    t11 checker released

--- src/main/java/com/epam/hakerrank/t11/Checker.java
+++ src/main/java/com/epam/hakerrank/t11/Checker.java
@@ -5,6 +5,12 @@ import java.util.Comparator;
 public class Checker implements Comparator<Player> {
     @Override
     public int compare(Player p1, Player p2) {
-        return 0;
+        int compareScore = Integer.compare(p1.score, p2.score);
+
+        if (compareScore == 0) {
+            return p1.name.compareTo(p2.name);
+        } else {
+            return -compareScore;
+        }
     }
 }
--- src/main/java/com/epam/hakerrank/t11/Player.java
+++ src/main/java/com/epam/hakerrank/t11/Player.java
@@ -8,4 +8,22 @@ public class Player{
         this.name = name;
         this.score = score;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Player player = (Player) o;
+
+        if (score != player.score) return false;
+        return name != null ? name.equals(player.name) : player.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + score;
+        return result;
+    }
 }
