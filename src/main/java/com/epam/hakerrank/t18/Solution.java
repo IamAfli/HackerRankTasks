5e6a27be9fd5436e34fa919649d5df94ef1230fe
Ilia Gusevskii <il.gusevskiye@gmail.com>
2017-07-31 09:17:51 +0300

    t18 refactored initial code

--- src/main/java/com/epam/hakerrank/t18/Solution.java
+++ src/main/java/com/epam/hakerrank/t18/Solution.java
@@ -6,8 +6,9 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int s = in.nextInt();
-        for(int a0 = 0; a0 < s; a0++){
+
+        int numberOfStairs = in.nextInt();
+        for(int i = 0; i < numberOfStairs; i++){
             int n = in.nextInt();
         }
     }
