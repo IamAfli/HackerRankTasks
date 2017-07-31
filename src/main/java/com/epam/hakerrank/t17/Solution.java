28f9f8f796bd5949f0f613c9dc2423127d876f39
Ilia Gusevskii <il.gusevskiye@gmail.com>
2017-07-31 08:45:03 +0300

    t17 solved

--- src/main/java/com/epam/hakerrank/t17/Solution.java
+++ src/main/java/com/epam/hakerrank/t17/Solution.java
@@ -4,13 +4,18 @@ import java.util.Scanner;
 
 public class Solution {
     public static int fibonacci(int n) {
-        throw new UnsupportedOperationException();
+        if (n == 0 || n == 1){
+            return n;
+        }
+
+        return fibonacci(n - 2) + fibonacci(n - 1);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         scanner.close();
+
         System.out.println(fibonacci(n));
     }
 }
