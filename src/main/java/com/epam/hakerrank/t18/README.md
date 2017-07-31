919d4ede2589e311869b43b16dc0dd18abbad5f7
Ilia Gusevskii <il.gusevskiye@gmail.com>
2017-07-31 09:16:59 +0300

    t18 initial commit

---
+++ src/main/java/com/epam/hakerrank/t18/README.md
@@ -0,0 +1,3 @@
+##Recursion: Davis' Staircase
+
+[task description](https://www.hackerrank.com/challenges/ctci-recursive-staircase)
---
+++ src/main/java/com/epam/hakerrank/t18/Solution.java
@@ -0,0 +1,14 @@
+package com.epam.hakerrank.t18;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        for(int a0 = 0; a0 < s; a0++){
+            int n = in.nextInt();
+        }
+    }
+}
