ab5d9e5b56b6aa39393fc7c0fd25ca6acd6fc787
Ilia Gusevskii <il.gusevskiye@gmail.com>
2017-07-31 10:18:12 +0300

    t18 solved. Tests fixed

--- src/main/java/com/epam/hakerrank/t18/Solution.java
+++ src/main/java/com/epam/hakerrank/t18/Solution.java
@@ -1,8 +1,12 @@
 package com.epam.hakerrank.t18;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Solution {
+
+    private static Map<Integer, Integer> map = new HashMap<>();
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -11,12 +15,21 @@ public class Solution {
         for(int i = 0; i < numberOfStairs; i++){
             int n = in.nextInt();
 
-            int result = findPossibleVariants(n);
-            System.out.println(result);
+            System.out.println(findPossibleVariants(n));
         }
     }
 
     static int findPossibleVariants(int n) {
-        throw new UnsupportedOperationException();
+        if(n < 0)
+            return 0;
+
+        if(n == 0)
+            return 1;
+
+        if(!map.containsKey(n)) {
+            int count = findPossibleVariants(n-1) + findPossibleVariants(n-2) + findPossibleVariants(n-3);
+            map.put(n, count);
+        }
+        return map.get(n);
     }
 }
--- src/test/java/com/epam/hakerrank/t18/SolutionTest.java
+++ src/test/java/com/epam/hakerrank/t18/SolutionTest.java
@@ -18,14 +18,14 @@ public class SolutionTest {
     @Test
     public void testCase2(){
         int result = Solution.findPossibleVariants(3);
-        int expected = 7;
+        int expected = 4;
 
         assertThat(result, is(expected));
     }
 
     @Test
     public void testCase3(){
-        int result = Solution.findPossibleVariants(4);
+        int result = Solution.findPossibleVariants(7);
         int expected = 44;
 
         assertThat(result, is(expected));
