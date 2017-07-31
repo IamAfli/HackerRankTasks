c7b92c845a9d1fd479f138cc289d16ffb2ba29c2
Ilia Gusevskii <il.gusevskiye@gmail.com>
2017-07-31 08:30:02 +0300

    t13 refactored initial code

--- src/main/java/com/epam/hakerrank/t13/Solution.java
+++ src/main/java/com/epam/hakerrank/t13/Solution.java
@@ -6,13 +6,15 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
-        for(int a0 = 0; a0 < t; a0++){
-            int m = in.nextInt();
-            int n = in.nextInt();
-            int a[] = new int[n];
-            for(int a_i=0; a_i < n; a_i++){
-                a[a_i] = in.nextInt();
+
+        int numberOfTimes = in.nextInt();
+        for (int i = 0; i < numberOfTimes; i++) {
+            int money = in.nextInt();
+            int numberOfFlavors = in.nextInt();
+
+            int[] flavors = new int[numberOfFlavors];
+            for (int j = 0; j < numberOfFlavors; j++) {
+                flavors[j] = in.nextInt();
             }
         }
     }
