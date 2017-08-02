36bf766f41d972808ed5512cda57f0da44fc32da
Ilia Gusevskii <il.gusevskiye@gmail.com>
2017-08-02 22:07:18 +0300

    t14 initial commit

---
+++ src/main/java/com/epam/hakerrank/t14/README.md
@@ -0,0 +1,3 @@
+##DFS: Connected Cell in a Grid
+
+[task description](https://www.hackerrank.com/challenges/ctci-connected-cell-in-a-grid)
---
+++ src/main/java/com/epam/hakerrank/t14/Solution.java
@@ -0,0 +1,18 @@
+package com.epam.hakerrank.t14;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int grid[][] = new int[n][m];
+        for(int grid_i=0; grid_i < n; grid_i++){
+            for(int grid_j=0; grid_j < m; grid_j++){
+                grid[grid_i][grid_j] = in.nextInt();
+            }
+        }
+    }
+}
