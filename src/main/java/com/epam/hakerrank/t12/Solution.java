9a444c3f580209667839ae82bba9e6207520c5f3
Ilia Gusevskii <il.gusevskiye@gmail.com>
2017-08-02 21:29:48 +0300

    t12 Solved, using recursion. Too slow

--- src/main/java/com/epam/hakerrank/t12/Solution.java
+++ src/main/java/com/epam/hakerrank/t12/Solution.java
@@ -5,8 +5,21 @@ import java.util.Scanner;
 public class Solution {
 
     static long countInversions(int[] arr) {
-        // Complete this function
-        throw new UnsupportedOperationException();
+        long result = 0;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i + 1] < arr[i]) {
+                int tmp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = tmp;
+
+                result++;
+
+                result += countInversions(arr);
+            }
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
@@ -14,11 +27,11 @@ public class Solution {
 
         int numberOfArrays = in.nextInt();
 
-        for(int i = 0; i < numberOfArrays; i++){
+        for (int i = 0; i < numberOfArrays; i++) {
             int arrayLength = in.nextInt();
             int[] array = new int[arrayLength];
 
-            for(int j = 0; j < arrayLength; j++){
+            for (int j = 0; j < arrayLength; j++) {
                 array[j] = in.nextInt();
             }
 
